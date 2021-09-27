180c7a7f49d9578c7afc7f75353cb1c3ba920a74
Arbas Ali <69846936+arbazali01@users.noreply.github.com>
2021-09-28 01:20:27 +0530

    Added a line of JS

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ let daylight = document.querySelector('.darkBright');
 let menuToggle = document.querySelector('.menuToggle');
 let body = document.querySelector('body');
 let nav = document.querySelector('.navi');
-
+let pic = document.querySelector('.pic');
 
 daylight.onclick = function(){
   body.classList.toggle('dark');
   daylight.classList.toggle('active');
+  pic.classList.toggle('active');
 }
 menuToggle.onclick = function(){
     menuToggle.classList.toggle('active');
